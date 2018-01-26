4f953643c3b64ca5f875e5aeed5534c190e61e25
lintao <mrqter@gmail.com>
2018-01-26 15:15:49 +0800

    update log

--- server/amazonCrawler/main.go
+++ server/amazonCrawler/main.go
@@ -13,29 +13,63 @@ import (
 var products []models.Product
 
 func main() {
-
-	for {
+	go func() {
 		if ur, err := getTopReviewUrl(); err == nil {
 			if users := crawler.CrawlerTopReviewUser(ur.Url, util.Country(ur.Country)); users != nil {
 				sendUsers(users)
-				for _, u := range users {
-					util.SetCountry(util.Country(u.Country))
-					ps := crawler.GetReviewListUrl(u)
-					for _, p := range ps {
-						products = append(products, p)
-						if len(products) >= 20 {
-							crawler.SendProduct(products)
-							products = make([]models.Product, 0)
-						}
-					}
-				}
 			}
-
 		} else {
 			util.Logger.Error(err.Error())
 		}
+	}()
+	for {
+		if users, err := getUsers(); err == nil {
+			for _, u := range users {
+				util.SetCountry(util.Country(u.Country))
+				ps := crawler.GetReviewListUrl(u)
+				for _, p := range ps {
+					products = append(products, p)
+					if len(products) >= 20 {
+						crawler.SendProduct(products)
+						products = make([]models.Product, 0)
+					}
+				}
+			}
+		}
+
 	}
 
+}
+
+func getUsers() ([]models.User, error) {
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "http://45.76.220.102:1323/", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	// Read Response Body
+	respBody, _ := ioutil.ReadAll(resp.Body)
+
+	var users []models.User
+
+	err = json.Unmarshal(respBody, &users)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
 
 func sendUsers(users []models.User) {
